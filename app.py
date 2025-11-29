3b563e3aba4b47cee77b2a09da3b343bb20158c0
mohan birajdar <mohanbirajdar41@gmail.com>
2025-11-29 11:52:18 +0530

    Update header for ATS Tracking System

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def input_pdf_setup(uploaded_file):
 ## Streamlit App
 
 st.set_page_config(page_title="  ATS Resume EXpert")
-st.header("CareerCatalyst's ATS Tracking System")
+st.header("MIT NEXUS's ATS Tracking System")
 input_text=st.text_area("Job Description: ",key="input")
 uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
 
